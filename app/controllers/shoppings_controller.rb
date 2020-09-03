6949b486996b6bfa4e52f15eb73d12b2284cd3d1
hiro_florist <64538711+hiro-sameshima@users.noreply.github.com>
2020-09-03 20:39:44 +0900

    購入ページへ商品の値を渡す

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,7 +1,7 @@
 class ShoppingsController < ApplicationController
 
   def index
-    # @item = Item.find(params[:id])
+    @item = Item.find(params[:format])
   end
 
   def create
--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,3 +1,7 @@
 class Shopping < ApplicationRecord
   validates :price, presence: true #金額の入力を必須にする記述
+
+
+belongs_to :user
+belongs_to  :item
 end
